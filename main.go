15fa4326d7d4671458f99eb350d3e93c34ee4392
sweeney <martin@sweeney.io>
2016-10-02 23:03:38 +0100

    OAuth token handshake

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/nlopes/slack"
 	"net/http"
 	"os"
 )
@@ -14,13 +15,41 @@ func main() {
 	}
 
 	http.HandleFunc("/", rootHandler)
+	//http.HandleFunc("/1:1", OneToOneHandler)
+	http.HandleFunc("/oauth", oauthHandler)
+
 	http.ListenAndServe(":"+port, nil)
 
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintln(w, "hello world")
+	fmt.Fprintln(w, "121bot: <a href=\"https://slack.com/oauth/authorize?scope=commands+users%3Aread\">Add to slack</a>")
 	return
+
+}
+
+// oauthHandler manages the OAuth 2.0 handshake and exchanges a code for an access token
+func oauthHandler(w http.ResponseWriter, r *http.Request) {
+
+	clientID := os.Getenv("SLACK_CLIENT_ID")
+	clientSecret := os.Getenv("SLACK_CLIENT_SECRET")
+	code := r.FormValue("code")
+
+	if code != "" {
+
+		authResponse, err := slack.GetOAuthResponse(clientID, clientSecret, code, "", false)
+
+		if err == nil {
+
+			fmt.Fprintf(w, "Team: %s(%s), Access token: %s, scope: %s", authResponse.TeamName, authResponse.TeamID, authResponse.AccessToken, authResponse.Scope)
+
+		} else {
+			http.Error(w, fmt.Sprintf("Error: %s", err.Error()), http.StatusBadRequest)
+		}
+
+	} else {
+		http.Error(w, "Missing code", http.StatusBadRequest)
+	}
 
 }
